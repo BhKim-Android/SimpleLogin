3bca488b75d4e92400574ba902749a4f2c8399c2
Byunghoon Kim <kidd890418@gmail.com>
2025-07-30 16:50:35 +0900

    first commit

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    `kotlin-dsl`
+}
+group = "com.kimbh.simplelogin.buildlogic"
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+kotlin {
+    compilerOptions {
+        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+    }
+}
+dependencies {
+    compileOnly(libs.android.gradlePlugin)
+    compileOnly(libs.compose.compiler.gradlePlugin)
+    compileOnly(libs.kotlin.gradlePlugin)
+    compileOnly(libs.hilt.gradlePlugin)
+}
+
