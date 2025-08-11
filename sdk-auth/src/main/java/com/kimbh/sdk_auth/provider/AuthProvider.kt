5f1907952d359ecab1156c782706403d4b712950
kimbh <kidd890418@gmail.com>
2025-08-11 23:37:59 +0900

    feat[Data] : Data Layer설정
    - suspend fun으로 login함수 생성
    - 리턴타입 : AuthResult<Auth>
    - :sdk-auth모듈에서 전달받은 결과(kakaoResponse)를 Auth로 Mapping

--- sdk-auth/src/main/java/com/kimbh/sdk_auth/provider/AuthProvider.kt
+++ sdk-auth/src/main/java/com/kimbh/sdk_auth/provider/AuthProvider.kt
@@ -1,9 +1,10 @@
 package com.kimbh.sdk_auth.provider
 
 import android.content.Context
+import com.kakao.sdk.auth.model.OAuthToken
 import com.kimbh.core.common.AuthResult
 import com.kimbh.sdk_auth.model.KakaoResponse
 
 interface AuthProvider {
-    fun login(callback: (AuthResult<KakaoResponse>) -> Unit)
+    suspend fun login(): KakaoResponse
 }
