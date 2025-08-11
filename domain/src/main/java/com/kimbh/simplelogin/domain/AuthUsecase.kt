f013983f29768b815106e72c69bb91437f3cfa6e
kimbh <kidd890418@gmail.com>
2025-08-11 23:35:27 +0900

    feat[Domain] : Domain Layer설정
    - 로그인 의도 구현.

--- domain/src/main/java/com/kimbh/simplelogin/domain/AuthUsecase.kt
+++ domain/src/main/java/com/kimbh/simplelogin/domain/AuthUsecase.kt
@@ -1,12 +1,14 @@
 package com.kimbh.simplelogin.domain
 
+import com.kimbh.core.common.AuthResult
+import com.kimbh.simplelogin.domain.model.Auth
 import com.kimbh.simplelogin.domain.repository.AuthRepository
 import javax.inject.Inject
 
 class AuthUsecase @Inject constructor(
     private val authRepository: AuthRepository
 ) {
-     fun login() {
-
+    operator suspend fun invoke(): AuthResult<Auth> {
+        return authRepository.login()
     }
 }
--- domain/src/main/java/com/kimbh/simplelogin/domain/model/Auth.kt
+++ domain/src/main/java/com/kimbh/simplelogin/domain/model/Auth.kt
@@ -1,5 +1,12 @@
 package com.kimbh.simplelogin.domain.model
 
+import java.util.Date
+
 data class Auth(
-    val token: String
+    val accessToken: String,
+    val accessTokenExpiresAt: Date,
+    val refreshToken: String,
+    val refreshTokenExpiresAt: Date,
+    val idToken: String? = null,
+    val scopes: List<String>? = null
 )
