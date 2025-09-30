cd77b964985adbddff037d6d998c8947c7cd2426
kimbh <kidd890418@gmail.com>
2025-09-30 17:23:09 +0900

    chore(:auth-kakao) : 카카오 초기화 모듈
    - 구현체와 추상화 인터페이스 연결 모듈 설계
    - @InthSet을 활용하여 멀티 바인딩 구조로 설계

--- simple-login-sdk/auth-kakao/src/main/java/com/kimbh/auth_kakao/di/KakaoStrategyModule.kt
+++ simple-login-sdk/auth-kakao/src/main/java/com/kimbh/auth_kakao/di/KakaoStrategyModule.kt
@@ -1,20 +1,17 @@
 package com.kimbh.auth_kakao.di
 
 import com.kimbh.auth_kakao.strategy.KakaoInitializerStrategy
-import com.kimbh.simple_login_sdk.strategy.InitializerStrategy
+import com.kimbh.core.strategy.InitializerStrategy
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import dagger.multibindings.IntoSet
-import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-abstract class KakaoStrategyModule {
-
+object KakaoStrategyModule {
     @Provides
-    @Singleton
     @IntoSet
     fun provideKakaoInitializerStrategy(): InitializerStrategy {
         return KakaoInitializerStrategy()
